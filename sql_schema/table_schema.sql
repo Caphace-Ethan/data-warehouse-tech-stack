0411b85634e1d374c4638188a1db99af91a29424
Caphace-Ethan <ethanyoung135@gmail.com>
2021-09-22 22:28:17 +0300

    Updated: table schema for stations

--- sql_schema/table_schema.sql
+++ sql_schema/table_schema.sql
@@ -1,20 +1,22 @@
-CREATE TABLE TweetInformation
+CREATE TABLE stations
 (
-    id SERIAL PRIMARY KEY,
-    created_at TEXT NOT NULL,
-    source VARCHAR(200) NOT NULL,
-    clean_text TEXT DEFAULT NULL,
-    polarity FLOAT DEFAULT NULL,
-    subjectivity FLOAT DEFAULT NULL,
-    language TEXT DEFAULT NULL,
-    favorite_count INT DEFAULT NULL,
-    retweet_count INT DEFAULT NULL,
-    original_author TEXT DEFAULT NULL,
-    followers_count INT DEFAULT NULL,
-    friends_count INT DEFAULT NULL,
-    hashtags TEXT DEFAULT NULL,
-    sensitivity TEXT DEFAULT NULL,
-    user_mentions TEXT DEFAULT NULL,
-    place TEXT DEFAULT NULL
+    id  INT PRIMARY KEY,
+    Fwy INT DEFAULT NULL,
+    Dir VARCHAR(3) DEFAULT NULL,
+    District INT DEFAULT NULL,
+    County INT DEFAULT NULL,
+    City FLOAT DEFAULT NULL,
+    State_PM FLOAT DEFAULT NULL,
+    Abs_PM FLOAT DEFAULT NULL,
+    Latitude FLOAT DEFAULT NULL,
+    Longitude FLOAT DEFAULT NULL,
+    Length FLOAT DEFAULT NULL,
+    Type VARCHAR(3) DEFAULT NULL,
+    Lanes INT DEFAULT NULL,
+    Name VARCHAR(20) DEFAULT NULL,
+    User_ID_1 VARCHAR(20) DEFAULT NULL,
+    User_ID_2 VARCHAR(20) DEFAULT NULL,
+    User_ID_3 VARCHAR(20) DEFAULT NULL,
+    User_ID_4 VARCHAR(20) DEFAULT NULL
 )
 -- ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
